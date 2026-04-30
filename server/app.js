2839007b09c01400a04daa00777cd6eaaf0b0390
hitheshamin24 <hitheshamin423@gmail.com>
2026-04-30 12:20:45 +0530

    setup express server with middleware and health check route

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+// create express app
+const app = express();
+
+// middleware
+// allow the react frontend to talk  to this server
+app.use(
+  cors({
+    origin: process.env.CLIENT_API,
+    credentials: true,
+  }),
+);
+
+// parse incoming JSON requests
+app.use(express.json());
+// parse URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/health", (req, res) => {
+  res.json({
+    message: "Niyam API is running",
+    status: "OK",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+module.exports = app;
