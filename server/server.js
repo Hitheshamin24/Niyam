ca141b742dacdc85435c8bee56469f8de3012b7d
hitheshamin24 <hitheshamin423@gmail.com>
2026-04-30 12:15:21 +0530

    setup express server with basic configuration and health route

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const app=require("./app")
+// load env variable
+dotenv.config();
+// connect to MongoDB
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Environment ${process.env.NODE_ENV}`);
+});
