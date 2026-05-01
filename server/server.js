4231640b499e2719b523f3e25ad107344f209501
hitheshamin24 <hitheshamin423@gmail.com>
2026-05-01 11:16:21 +0530

    fix async execution order by wrapping server start in async function

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const app=require("./app")
+const app = require("./app");
 // load env variable
 dotenv.config();
 // connect to MongoDB
-connectDB();
+const startServer = async () => {
+  await connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-  console.log(`Environment ${process.env.NODE_ENV}`);
-});
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`Environment ${process.env.NODE_ENV}`);
+  });
+};
+startServer();
